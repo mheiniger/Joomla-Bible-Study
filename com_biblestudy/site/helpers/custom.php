96203e29c9419544e82ceb6bae95467dcece4add
Brent Cordis <skeebrent@gmail.com>
2013-10-19 18:14:30 -0500

    close #312  This makes Custom content render.

--- com_biblestudy/site/helpers/custom.php
+++ com_biblestudy/site/helpers/custom.php
@@ -39,7 +39,7 @@ class JBSMCustom
 			$braceend   = strpos($custom, '}');
 			$subcustom  = substr($custom, ($bracebegin + 1), (($braceend - $bracebegin) - 1));
 
-			if (!$rowid)
+			if (!$rowid || $rowid == 24)
 			{
 				$rowid = $this->getElementnumber($subcustom);
 			}
